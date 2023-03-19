85622ab4e2062aa7d550acac542668ebe8f6fbc3
luskoeqk <77803201+luskoeqk@users.noreply.github.com>
2023-03-19 22:09:57 +0200

    Update FormElements.ts

--- next-app/src/styles/FormElements.ts
+++ next-app/src/styles/FormElements.ts
@@ -4,14 +4,25 @@ import styled from 'styled-components';
 export const StudentFormCU = styled.div`
 
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(min(23rem, 100%), 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(min(22rem, 100%), 1fr));
     grid-gap: 30px;
-    width: 100vw;
+    width: 100%;
+
+    @media (max-width: 768px) {
+        width: 100vw;
+        
+    }
 `
 
 export const StudentFormWrapper = styled.div`
     box-shadow: 1px 1px 8px 2px #1976d2;
     border-radius: 15px;
     padding: 100px;
-    width: 100vw;
+    width: 100%;
+
+
+    @media (max-width: 768px) {
+        width: 140vw;
+        padding: 30px;
+    }
 `
