30b635163ac65ea3c830c2e733043430877bcba8
luskoeqk <lusy2017@gmail.com>
2023-06-07 13:40:05 +0300

    edit

--- next-app/src/pages/api/students/masters/get_all.ts
+++ next-app/src/pages/api/students/masters/get_all.ts
@@ -12,10 +12,6 @@ export default async function getStudent(req: NextApiRequest, res: NextApiRespon
         connectMongoDB();
         console.log('Mongoose: getting docs for Student Master...');
 
-        const StudentDataSorted = await masterStudent.find().sort({ dateOfCreation: -1 });
-        
-        console.log(`Mongoose: Student Master --> Mongoose found items ${StudentDataSorted.length}`);
-        res.status(200).json(StudentDataSorted);
     } catch (error) {
         
         res.status(500).send(error);
