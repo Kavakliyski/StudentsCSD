94b3ae0a88d1b7f47fd684e6a05e0c811da2e32f
luskoeqk <lusy2017@gmail.com>
2023-06-07 13:43:07 +0300

    merge conflict resolve

--- next-app/src/pages/api/students/masters/get_all.ts
+++ next-app/src/pages/api/students/masters/get_all.ts
@@ -10,8 +10,12 @@ export default async function getStudent(req: NextApiRequest, res: NextApiRespon
     try {
 
         connectMongoDB();
-        console.log('Mongoose: getting docs for Student Master...');
+        console.log('Mongoose: getting docs for Students MASTER...');
 
+        const SortedMastersDocs = await masterStudent.find().sort({ dateOfCreation: -1 });
+        
+        console.log(`Mongoose: Students MASTER --> Mongoose found items ${SortedMastersDocs.length}`);
+        res.status(200).json(SortedMastersDocs);
     } catch (error) {
         
         res.status(500).send(error);
