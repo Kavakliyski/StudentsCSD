14bb86b0a142471b504a9189ff75511310f054c5
luskoeqk <lusy2017@gmail.com>
2023-06-23 13:28:14 +0300

    test api hello

--- next-app/src/pages/api/hello.ts
+++ next-app/src/pages/api/hello.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import admin from 'firebase-admin';
-
+import { firebaseServiceAccount } from '../../../utils/firebase-adminsdk';
 
 type Data = {
   name?: string;
@@ -9,19 +9,23 @@ type Data = {
   error?: any;
 }
 
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 
   if (!admin.apps.length) {
-    const serviceAccount = require('../../../utils/vuzf-studentscsd-firebase-adminsdk.json');
 
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(firebaseServiceAccount)
     });
   }
 
   const token = req.headers.authorization
 
   if (!token) {
+
+    console.log('====================================');
+    // console.log(firebaseServiceAccount);
+    console.log('====================================');
 
     return res.status(401).json({ error: 'Unauthorized' });
   } else {
