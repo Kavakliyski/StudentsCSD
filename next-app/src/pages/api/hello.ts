1353f8e9bf2b9d749a580cb1c734d6551834a9c6
luskoeqk <lusy2017@gmail.com>
2023-06-26 10:13:15 +0300

    todo: branch

--- next-app/src/pages/api/hello.ts
+++ next-app/src/pages/api/hello.ts
@@ -13,41 +13,5 @@ type Data = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 
-    if (!admin.apps.length) {
-
-        admin.initializeApp({
-            credential: admin.credential.cert(firebaseServiceAccount)
-        });
-    }
-
-    const token = req.headers.authorization
-
-    if (!token) {
-        return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-
-    try {
-        const decodedToken = await admin.auth().verifyIdToken(token);
-        const userId = decodedToken.uid;
-        const userEmail = decodedToken.email;
-
-        console.log('====================================');
-        console.log(userId);
-        console.log(userEmail);
-        
-        if (userEmail) checkEmailExists(userEmail)
-            .then((exists) => {
-                console.log(`Email exists: ${exists}`);
-            })
-            .catch((error) => {
-                console.error('Error checking email:', error);
-            });
-        console.log('====================================');
-
-        res.status(200).json({ message: 'Success' });
-    } catch (error) {
-        console.error('Token verification failed:', error);
-        res.status(401).json({ error: 'Invalid token' });
-    }
+    res.status(200).send({ message: 'hell world' });
 };
