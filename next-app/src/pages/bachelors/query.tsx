7afaf7df9a93f0acced6ba3d6a600876aa4b56ef
luskoeqk <lusy2017@gmail.com>
2023-07-05 16:37:45 +0300

    Querry fixes

--- next-app/src/pages/bachelors/query.tsx
+++ next-app/src/pages/bachelors/query.tsx
@@ -2,7 +2,7 @@
 import { PageConfig } from "@/styles/PagesConfigElements";
 
 // next
-import { useState } from 'react';
+import { useState } from "react";
 
 // axios
 import axios from "axios";
@@ -15,20 +15,19 @@ import BachelorStudentsTable from "@/components/studentsTable/BachelorStudentsTa
 
 // Material UI
 import { Box, Button, TextField } from "@mui/material";
-import LinearProgress from '@mui/material/LinearProgress';
+import LinearProgress from "@mui/material/LinearProgress";
 
 // ExcelJS
 import ExportXLSXButtonBachelors from "@/components/ExportToXLSX/ExportXLSXButtonBachelors";
 
-
-const ENTRIES_API_URL = '/api/students/bachelors/get_entries';
-
+const ENTRIES_API_URL = "/api/students/bachelors/get_entries";
 
 export default function Query() {
-
-    const [studentsGetEntryData, setStudentsGetEntryData] = useState<IStudentBachelor[]>([]);
+    const [studentsGetEntryData, setStudentsGetEntryData] = useState<
+        IStudentBachelor[]
+    >([]);
     const [submitted, setSubmitted] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);                   // wait for fetch request
+    const [isLoading, setIsLoading] = useState(true); // wait for fetch request
 
     const [params, setParams] = useState<any>({});
 
@@ -41,87 +40,197 @@ export default function Query() {
         axios
             .get<IStudentBachelor[]>(ENTRIES_API_URL, { params })
             .then((res) => {
-                setStudentsGetEntryData(res.data)
+                setStudentsGetEntryData(res.data);
                 setIsLoading(false);
             })
             .catch((err) => {
-                console.log("Error fetching students:", err)
+                console.log("Error fetching students:", err);
                 setIsLoading(false);
             });
     };
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setParams({
-            ...params,
-            [e.target.name]: e.target.value,
-        });
+        const { name, value } = e.target;
+
+        if (value.trim() === "") {
+            // Exclude empty query parameters
+            const updatedParams = { ...params };
+            delete updatedParams[name];
+            setParams(updatedParams);
+        } else {
+            setParams({
+                ...params,
+                [name]: value,
+            });
+        }
     };
 
-    console.log(params)
+    console.log(params);
 
     return (
         <PageConfig>
-
             <h1>Търсене / Query, Бакалаври</h1>
 
             <ExportXLSXButtonBachelors data={studentsGetEntryData} />
 
             <div>
-
                 <h2>Търспи по:</h2>
 
                 <Box
                     onSubmit={handleSubmit}
                     component="form"
                     sx={{
-                        '& > :not(style)': { m: 1, width: '25ch' },
+                        "& > :not(style)": { m: 1, width: "25ch" },
                     }}
                     noValidate
                     autoComplete="off"
                 >
-                    <TextField label="Отличителност" name="distinction" variant="standard" onChange={handleChange} />
-                    <TextField label="Факултетен номер" name="faculty_number" variant="standard" onChange={handleChange} />
-                    <TextField label="Статус на кск" name="status_of_ksk" variant="standard" onChange={handleChange} />
-                    <TextField label="№ на заповед за записване" name="n_of_enrollment_order" variant="standard" onChange={handleChange} />
-                    <TextField label="Име Презиме Фамилия" name="names" variant="standard" onChange={handleChange} />
-                    <TextField label="ЕГН" name="egn" variant="standard" onChange={handleChange} />
-                    <TextField label="Телефон" name="phone_number" variant="standard" onChange={handleChange} />
-                    <TextField label="Имейл" name="email" variant="standard" onChange={handleChange} />
+                    <TextField
+                        label="Отличителност"
+                        name="distinction"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Факултетен номер"
+                        name="faculty_number"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Статус на кск"
+                        name="status_of_ksk"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="№ на заповед за записване"
+                        name="n_of_enrollment_order"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Име Презиме Фамилия"
+                        name="names"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="ЕГН"
+                        name="egn"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Телефон"
+                        name="phone_number"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Имейл"
+                        name="email"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
 
-                    <TextField label="Професионално направление / квалификация" name="professional_qualification" variant="standard" onChange={handleChange} />
-                    <TextField label="Потвърждение от Нацид" name="confirmation_by_nacid" variant="standard" onChange={handleChange} />
-                    <TextField label="Желана Специалност" name="desired_major" variant="standard" onChange={handleChange} />
-                    <TextField label="Желана форма" name="desired_shape" variant="standard" onChange={handleChange} />
-                    <TextField label="Курс" name="bachelor_course" variant="standard" onChange={handleChange} />
-                    <TextField label="Продължителност семестри" name="duration_semesters" variant="standard" onChange={handleChange} />
-                    <TextField label="Начин на обучение" name="form_of_study" variant="standard" onChange={handleChange} />
-                    <TextField label="Период на подаване в АдминУни" name="submission_period_in_adminuni" variant="standard" onChange={handleChange} />
-                    <TextField label="Учебна година" name="school_year" variant="standard" onChange={handleChange} />
-                    <TextField label="Отстъпка" name="discount" variant="standard" onChange={handleChange} />
+                    <TextField
+                        label="Професионално направление / квалификация"
+                        name="professional_qualification"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Потвърждение от Нацид"
+                        name="confirmation_by_nacid"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Желана Специалност"
+                        name="desired_major"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Желана форма"
+                        name="desired_shape"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Курс"
+                        name="bachelor_course"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Продължителност семестри"
+                        name="duration_semesters"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Начин на обучение"
+                        name="form_of_study"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Период на подаване в АдминУни"
+                        name="submission_period_in_adminuni"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Учебна година"
+                        name="school_year"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Отстъпка"
+                        name="discount"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
 
-                    <TextField label="КОХОРТ В МООДЛЕ " name="cohort_in_moodle" variant="standard" onChange={handleChange} />
-                    <TextField label="Студентски имейл" name="university_email" variant="standard" onChange={handleChange} />
-                    <TextField label="_ID" name="_id" variant="standard" onChange={handleChange} />
+                    <TextField
+                        label="КОХОРТ В МООДЛЕ "
+                        name="cohort_in_moodle"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Студентски имейл"
+                        name="university_email"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="_ID"
+                        name="_id"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
 
                     <br />
-                    <Button variant="contained" type="submit">Търси</Button>
+                    <Button variant="contained" type="submit">
+                        Търси
+                    </Button>
                 </Box>
             </div>
 
             {/* Render the search results */}
-            {
-                submitted &&
-                    isLoading ? (
-                    <>
-                        <Box sx={{ width: '99%' }}>
-                            <LinearProgress />
-                        </Box>
-                    </>
-
-                ) : (
-                    <BachelorStudentsTable studentsGetData={studentsGetEntryData} />
-                )
-            }
+            {submitted && isLoading ? (
+                <>
+                    <Box sx={{ width: "99%" }}>
+                        <LinearProgress />
+                    </Box>
+                </>
+            ) : (
+                <BachelorStudentsTable studentsGetData={studentsGetEntryData} />
+            )}
         </PageConfig>
-    )
+    );
 }
--- next-app/src/pages/masters/query.tsx
+++ next-app/src/pages/masters/query.tsx
@@ -2,7 +2,7 @@
 import { PageConfig } from "@/styles/PagesConfigElements";
 
 // next
-import { useState } from 'react';
+import { useState } from "react";
 
 // axios
 import axios from "axios";
@@ -15,21 +15,20 @@ import MasterStudentsTable from "@/components/studentsTable/MasterStudentsTable"
 
 // Material UI
 import { Box, Button, TextField } from "@mui/material";
-import LinearProgress from '@mui/material/LinearProgress';
+import LinearProgress from "@mui/material/LinearProgress";
 
-// ExcelJS 
+// ExcelJS
 import ExportXLSXButtonMasters from "@/components/ExportToXLSX/ExportXLSXButtonMasters";
 
-
 // const ENTRIES_API_URL = `${process.env.NEXT_PUBLIC_MONGODB_URL}/api/students/masters/get_entries`;
-const ENTRIES_API_URL = '/api/students/masters/get_entries';
-
+const ENTRIES_API_URL = "/api/students/masters/get_entries";
 
 export default function Query() {
-
-    const [studentsGetEntryData, setStudentsGetEntryData] = useState<IStudent[]>([]);
+    const [studentsGetEntryData, setStudentsGetEntryData] = useState<
+        IStudent[]
+    >([]);
     const [submitted, setSubmitted] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);                   // wait for fetch request
+    const [isLoading, setIsLoading] = useState(true); // wait for fetch request
 
     const [params, setParams] = useState<any>({});
 
@@ -42,81 +41,165 @@ export default function Query() {
         axios
             .get<IStudent[]>(ENTRIES_API_URL, { params })
             .then((res) => {
-                setStudentsGetEntryData(res.data)
+                setStudentsGetEntryData(res.data);
                 setIsLoading(false);
             })
             .catch((err) => {
-                console.log("Error fetching students:", err)
+                console.log("Error fetching students:", err);
                 setIsLoading(false);
-            })
+            });
     };
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setParams({
-            ...params,
-            [e.target.name]: e.target.value,
-        });
-    };
+        const { name, value } = e.target;
 
+        if (value.trim() === "") {
+            // Exclude empty query parameters
+            const updatedParams = { ...params };
+            delete updatedParams[name];
+            setParams(updatedParams);
+        } else {
+            setParams({
+                ...params,
+                [name]: value,
+            });
+        }
+    };
 
     return (
         <PageConfig>
-
             <h1>Търсене / Query, Магистри</h1>
 
             <ExportXLSXButtonMasters data={studentsGetEntryData} />
 
             <div>
-
                 <h2>Търспи по:</h2>
 
                 <Box
                     onSubmit={handleSubmit}
                     component="form"
                     sx={{
-                        '& > :not(style)': { m: 1, width: '25ch' },
+                        "& > :not(style)": { m: 1, width: "25ch" },
                     }}
                     noValidate
                     autoComplete="off"
                 >
-                    <TextField label="Отличителност" name="distinction" variant="standard" onChange={handleChange} />
-                    <TextField label="Факултетен номер" name="faculty_number" variant="standard" onChange={handleChange} />
-                    <TextField label="Статус на кск" name="status_of_ksk" variant="standard" onChange={handleChange} />
-                    <TextField label="№ на заповед за записване" name="n_of_enrollment_order" variant="standard" onChange={handleChange} />
-                    <TextField label="Име Презиме Фамилия" name="names" variant="standard" onChange={handleChange} />
-                    <TextField label="ЕГН" name="egn" variant="standard" onChange={handleChange} />
-                    <TextField label="Телефон" name="phone_number" variant="standard" onChange={handleChange} />
-                    <TextField label="Имейл" name="email" variant="standard" onChange={handleChange} />
+                    <TextField
+                        label="Отличителност"
+                        name="distinction"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Факултетен номер"
+                        name="faculty_number"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Статус на кск"
+                        name="status_of_ksk"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="№ на заповед за записване"
+                        name="n_of_enrollment_order"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Име Презиме Фамилия"
+                        name="names"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="ЕГН"
+                        name="egn"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Телефон"
+                        name="phone_number"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Имейл"
+                        name="email"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
 
-                    <TextField label="Професионално направление / квалификация" name="professional_qualification" variant="standard" onChange={handleChange} />
-                    <TextField label="Потвърждение от Нацид" name="confirmation_by_nacid" variant="standard" onChange={handleChange} />
-                    <TextField label="Желана Специалност" name="desired_major" variant="standard" onChange={handleChange} />
-                    <TextField label="Желана форма" name="desired_shape" variant="standard" onChange={handleChange} />
-                    <TextField label="Продължителност на обучение" name="length_of_study" variant="standard" onChange={handleChange} />
-                    <TextField label="КОХОРТ В МООДЛЕ " name="cohort_in_moodle" variant="standard" onChange={handleChange} />
+                    <TextField
+                        label="Професионално направление / квалификация"
+                        name="professional_qualification"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Потвърждение от Нацид"
+                        name="confirmation_by_nacid"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Желана Специалност"
+                        name="desired_major"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Желана форма"
+                        name="desired_shape"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="Продължителност на обучение"
+                        name="length_of_study"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="КОХОРТ В МООДЛЕ "
+                        name="cohort_in_moodle"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
 
-                    <TextField label="Студентски имейл" name="university_email" variant="standard" onChange={handleChange} />
-                    <TextField label="_ID" name="_id" variant="standard" onChange={handleChange} />
+                    <TextField
+                        label="Студентски имейл"
+                        name="university_email"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        label="_ID"
+                        name="_id"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
 
                     <br />
-                    <Button variant="contained" type="submit">Търси</Button>
+                    <Button variant="contained" type="submit">
+                        Търси
+                    </Button>
                 </Box>
             </div>
 
             {/* Render the search results */}
-            {
-                submitted &&
-                    isLoading ? (
-                    <>
-                        <Box sx={{ width: '99%' }}>
-                            <LinearProgress />
-                        </Box>
-                    </>
-
-                ) : (
-                    <MasterStudentsTable studentsGetData={studentsGetEntryData} />
-                )
-            }
+            {submitted && isLoading ? (
+                <>
+                    <Box sx={{ width: "99%" }}>
+                        <LinearProgress />
+                    </Box>
+                </>
+            ) : (
+                <MasterStudentsTable studentsGetData={studentsGetEntryData} />
+            )}
         </PageConfig>
     );
 }
